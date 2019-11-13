e53e5381a8a7d82cb5f827c6b7d1c660474e1261
Colin Ries <colin.ries@gmail.com>
2019-11-13 23:58:49 +0000

    added time delay at start and end of show

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from room import Room
 from edge import Edge
 from Compass import DataFlow, Direction
+import time
 from config import build_living_room
 
 living_room = build_living_room()
 
 living_room.hue_off()
-living_room.rainbow_ceiling_only(speed=10, include_vertical=True)
+time.sleep(2)
+living_room.rainbow_ceiling_only(speed=60, include_vertical=True)
+time.sleep(1)
 living_room.leds_off()
+time.sleep(1)
 living_room.hue_on()
