6290a25347aaeee59a5961c94a07f589e1e3ebd6
Colin Ries <colin.ries@gmail.com>
2019-11-25 04:15:08 +0000

    added a basic idle rainbow effect

---
+++ basic_rainbow_4 times_infinite.py
@@ -0,0 +1,7 @@
+import time
+from config import build_living_room
+
+living_room = build_living_room()
+time.sleep(2)
+while True:
+    living_room.hue_span_color_cylce(living_room.build_list_horizontal_circle(), compress=4, speed=10, cycles=1000)
